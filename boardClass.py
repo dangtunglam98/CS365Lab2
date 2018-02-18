e82879a5e7e7b07d33cda51d19114287a6ed1527
Anh Dang <aqdang16@earlham.edu>
2018-02-17 23:06:38 -0500

    asf

--- boardClass.py
+++ boardClass.py
@@ -42,58 +42,36 @@ class Board:
 				if self.board[i][j] == "O":
 					self.whitePos.append((i,j))
 	
-	# def initial_state(self,rowsNum,colsNum,piecesNum):
-	# 	"""Return initial_state of the board, before any action was made"""
-	# 	self.rowsNum = rowsNum
-	# 	self.colsNum = colsNum
-	# 	self.piecesNum = piecesNum
-
-	# 	for whitepiece in range(self.piecesNum):
-	# 		row = []
-	# 		for c in range(self.colsNum):
-	# 			row.append("X")
-	# 			self.blackNum += 1
-	# 		self.board.append(row)
-
-	# 	for r in range(self.rowsNum - (self.piecesNum*2)):
-	# 		row = []
-	# 		for c in range(self.colsNum):
-	# 			row.append(".")
-	# 		self.board.append(row)
-
-	# 	for blackpiece in range(self.piecesNum):
-	# 		row = []
-	# 		for c in range(self.colsNum):
-	# 			row.append("O")
-	# 			self.whiteNum += 1
-	# 		self.board.append(row)
-
-	def move(self,start,end):
+	def transition(self,move):
 		"""Move a piece to a certain position"""
+		start, end = move
 		xstart, ystart = start
 		xend, yend = end
 		if self.board[xend][yend] == "O":
 			self.whiteNum -= 1
 		if self.board[xend][yend] == "X":
 			self.blackNum -= 1
-		self.board[xend][yend] = self.board[xstart][ystart]
+		endPos = self.board[xend][yend]
+		startPos = self.board[xstart][ystart]
+		self.board[xend][yend] = startPos
 		self.board[xstart][ystart] = "."
+		return self
 
 	def display_state(self):
 		"""Return the current state of the board"""
 		for row in self.board:
 			print(' '.join(row))
 
-	def ending_state(self):
+	def terminal_test(self):
 		"""Return the winner of the game"""
 		if self.blackNum == 0 or ("O" in self.board[0]):
-			print("White wins")
+			
 			return True
 		elif self.whiteNum == 0 or ("X" in self.board[self.rowsNum - 1]):
-			print("Black wins")
+			
 			return True
 		else:
-			print("Not ending state")
+			
 			return False
 
 	def move_generator(self,player):
@@ -120,6 +98,14 @@ class Board:
 					moveset[(row,col)].append((row-1,col-1))
 		return moveset
 
+	def moveList(self,player):
+		moveList = []
+		for key,vals in self.move_generator(player).items():
+			for end in vals:
+				moveList.append((key,end))
+		return moveList
+
+
 	def display_pos_move(self,moveset):
 		"""Return the possible move on the board"""
 		for keys, values in moveset.items():
@@ -127,14 +113,7 @@ class Board:
 				(x,y) = i
 				self.board[x][y] = "P"
 
-a = Board(5,4,1)
-print(a.whitePos)
-print(a.blackPos)
-print(a.move_generator("O"))
-# a.move((1,0),(2,0))
-# a.move((1,5),(3,5))
-# a.move((6,4),(4,4))
-# print("   ")
-# print(a.move_generator("X"))
-#a.display_pos_move(a.move_generator("X"))
-a.display_state()
+# a = Board(5,5,1)
+# a.transition(((0,0),(1,0))).display_state()
+# print("OK")
+# a.display_state()
--- gamePlay.py
+++ gamePlay.py
@@ -1,4 +1,5 @@
 from random import *
+from boardClass import Board
 
 def evasive(board,player):
 	if player == "X":
@@ -11,3 +12,104 @@ def conqueror(board,player):
 		return (0 - board.whiteNum + random())
 	else:
 		return (0 - board.blackNum + random())
+
+def switchPlayer(player):
+	if player == "X":
+		return "O"
+	else:
+		return "X"
+
+def minimax(utilFunc,board,player):
+	# if board.terminal_test():
+	# 	return None
+	moves = board.moveList(player)
+	board2 = board
+	best_move = moves[0]
+	best_score = utilFunc(board2.transition(moves[0]), player)
+	for move in moves:
+		clone = board.transition(move)
+		score = minplay(utilFunc,clone,player,0)
+		if score > best_score:
+			best_move = move
+			best_score = score
+	return best_move
+
+def minplay(utilFunc,board,player,depth):
+	if board.terminal_test() or (depth > 3):
+		return utilFunc(board, player)
+	moves = board.moveList(switchPlayer(player))
+	best_score = utilFunc(board.transition(moves[0]), player)
+	for move in moves:
+		clone = board.transition(move)
+		score = maxplay(utilFunc,clone,player,depth+1)
+		if score < best_score:
+			best_move = move
+			best_score = score
+	return best_score
+
+def maxplay(utilFunc,board,player,depth):
+	if board.terminal_test() or (depth > 3):
+		return utilFunc(board, player)
+	moves = board.moveList(player)
+	best_score = utilFunc(board.transition(moves[0]), player)
+	for move in moves:
+		clone = board.transition(move)
+		score = minplay(utilFunc,clone,player,depth+1)
+		if score > best_score:
+			best_move = move
+			best_score = score
+	return best_score
+
+def play_game(heuristic_white,heuristic_black,board_state,turn):
+	if board_state.terminal_test():
+		return board_state, turn
+	return white_turn(heuristic_white,heuristic_black,board_state,turn+1)
+
+
+def white_turn(heuristic_white,heuristic_black,board_state,turn):
+	if board_state.terminal_test():
+		return board_state, turn
+	move = minimax(heuristic_white,board_state,"O")
+	return black_turn(heuristic_white,heuristic_black,board_state.transition(move),turn+1)
+
+def black_turn(heuristic_white,heuristic_black,board_state,turn):
+	if board_state.terminal_test():
+		return board_state, turn
+	move = minimax(heuristic_black,board_state,"X")
+	return white_turn(heuristic_white,heuristic_black,board_state.transition(move),turn+1)
+
+# class Node(object):
+# 	def __init__(self, parent = None, action, board, util, player, depth):
+# 		self.parent = parent
+# 		self.child = []
+# 		self.action = action
+# 		self.board = board
+# 		self.utility = util(self.state, player)
+# 		self.depth = depth
+		
+# 		for key,vals in board.move_generator(player).items():
+# 			for end in vals:
+# 				self.child.append(Node(self, (player,key,end), self.board.move(key,end), util, player, self.depth + 1)
+
+
+a = Board(5,5,1)
+a.display_state()
+print(a.terminal_test())
+print("OK")
+#for i in range(2):
+#print(minimax(conqueror,a,"X"))
+a = a.transition(minimax(conqueror,a,"X"))
+a.display_state()
+print(minimax(evasive,a,"O"))
+a = a.transition(minimax(evasive,a,"O"))
+a.display_state()
+# play_game(evasive,evasive,a,0)[0].display_state()
+# print(play_game(evasive,evasive,a,0)[1])
+
+
+
+
+
+
+	
+
